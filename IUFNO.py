23efc24f3ad966b4e908799fb9f3f49828a9a915
Dwyer Deighan <dwyerdei@buffalo.com>
2025-11-29 02:26:03 -0500

    first draft of adapation of (IU)FNO4d

--- IUFNO.py
+++ IUFNO.py
@@ -19,9 +19,10 @@ import os
 torch.manual_seed(123)
 np.random.seed(123)
 
-
 ################################################################
 # fourier layers
+
+import pytorch_lightning as L
 
 class SpectralConv4d(nn.Module):
     def __init__(self, in_channels, out_channels, modes1, modes2, modes3, modes4):
@@ -52,7 +53,7 @@ class SpectralConv4d(nn.Module):
     def forward(self, x):
         batchsize = x.shape[0]
         #Compute Fourier coeffcients up to factor of e^(- something constant)
-        x_ft = torch.fft.rfftn(x, dim=[-4,-3,-2,-1])
+        x_ft = torch.fft.rfftn(x, dim=[-4,-3,-2,-1]) # Dwyer: The "w" dimension is indeed convolved over
 
         # Multiply relevant Fourier modes
         out_ft = torch.zeros(batchsize, self.out_channels, x.size(-4), x.size(-3), x.size(-2), x.size(-1), dtype=torch.cfloat, device=x.device)
@@ -65,6 +66,7 @@ class SpectralConv4d(nn.Module):
         x = torch.fft.irfftn(out_ft, s=(x.size(-4), x.size(-3), x.size(-2), x.size(-1)))
         return x
 
+# Dwyer: GOTCHA: this flattens the spatial dimensions into a single dimension then applies non-unit CNN layers!
 class U_net(nn.Module):
     def __init__(self, input_channels, output_channels, kernel_size, dropout_rate):
         super(U_net, self).__init__()
@@ -85,6 +87,7 @@ class U_net(nn.Module):
 
     def forward(self, x):
         batchsize, width = x.shape[0], x.shape[1]
+        spatial = x.shape[2:]
         out_conv1 = self.conv1(x.view(batchsize, width, -1))
         out_conv2 = self.conv2_1(self.conv2(out_conv1))
         out_conv3 = self.conv3_1(self.conv3(out_conv2))
@@ -94,8 +97,9 @@ class U_net(nn.Module):
         out_deconv1 = self.deconv1(concat2)
         concat1 = torch.cat((out_conv1, out_deconv1), 1)
         out_deconv0 = self.deconv0(concat1)
-        concat0 = torch.cat((x, out_deconv0.view(batchsize, width, 32, 33, 16, 3)), 1)
-        out = self.output_layer(concat0.view(concat0.shape[0], concat0.shape[1], -1)).view(batchsize, width, 32, 33, 16, 3)
+        #concat0 = torch.cat((x, out_deconv0.view(batchsize, width, 32, 33, 16, 3)), 1) # Dwyer: x=32, y=33, z=16, w=3
+        concat0 = torch.cat((x, out_deconv0.view(batchsize, width, *spatial)), 1)
+        out = self.output_layer(concat0.view(concat0.shape[0], concat0.shape[1], -1)).view(batchsize, width, *spatial)
 
         return out
 
@@ -118,61 +122,76 @@ class U_net(nn.Module):
         return nn.Conv1d(input_channels, output_channels, kernel_size=kernel_size,
                           stride=stride, padding=(kernel_size - 1) // 2)
 
+from lightning_utils import BasicLightningRegressor, ToggleableGroupNorm
 
-class FNO4d(nn.Module):
-    def __init__(self, modes1, modes2, modes3, modes4, width, nlayer):
-        super(FNO4d, self).__init__()
-
-        """
-        input: the solution of the first 5 timesteps + 3 locations (u(1, x, y), ..., u(10, x, y),  x, y, t). It's a constant function in time, except for the last index.
-
-        output: the solution of the next  timestep
+# Dwyer: this is the main model
+class IUFNO4d(BasicLightningRegressor):
+    def __init__(self, in_channels, out_channels, hidden_channels, k_modes:list|int=32, k_size:int=3, n_layers=4, ndims=3, hidden_norm_groups=0, out_norm_groups=0):
+        super(IUFNO4d, self).__init__()
 
         """
+        input: the solution of the first 1 timesteps + 4 locations (u(1, x, y, z), ..., u(1, x, y, z),  x, y, z, t). It's a constant function in time, except for the last index.
+        output: the solution of the next timestep
+        """
 
-        self.modes1 = modes1
-        self.modes2 = modes2
-        self.modes3 = modes3
-        self.modes4 = modes4
-        self.width = width
-        self.fc0 = nn.Linear(9, self.width)  # 5+4 (u(1, x, y), ..., u(5, x, y), x,y,z,t)
-        self.nlayer = nlayer
+        if ndims != 3: raise NotImplementedError("IUFNO4d only supports ndims=3 (3D spatial dimensions)")
+        if in_channels!=out_channels: raise NotImplementedError("in_channels must equal out_channels, (they are held fixed in the W/component dimension)")
 
-        self.convlayer = nn.ModuleList([SpectralConv4d(self.width, self.width, self.modes1, self.modes2, self.modes3, self.modes4).cuda() for i in range(1)])
-        self.w = nn.ModuleList([nn.Conv1d(self.width, self.width, 1).cuda() for i in range(1)])
-        self.u = nn.ModuleList([U_net(self.width, self.width, 3, 0).cuda() for i in range(1)])
+        if type(k_modes) is int:
+            k_modes = [k_modes]*ndims
+        assert type(k_size) is int and k_size > 0
+        self.modes1, self.modes2, self.modes3 = k_modes
+        self.modes4 = in_channels # 3 for u,v,w (we need to cram channels into the modes4 dimension because of their crazy component convolution)
+        self.width = hidden_channels
+        input_time_steps = 1
+        self.fc0 = nn.Linear(input_time_steps + 4, self.width)  # 1+4 (u(1, x, y), ..., u(1, x, y), x,y,z,t)
+        self.nlayer = n_layers
+
+        self.convlayer = nn.ModuleList([SpectralConv4d(self.width, self.width, self.modes1, self.modes2, self.modes3, self.modes4) for i in range(1)])
+        self.w = nn.ModuleList([nn.Conv1d(self.width, self.width, 1) for i in range(1)]) # Dwyer: this is literally the W bias from the FNO paper!
+        self.u = nn.ModuleList([U_net(self.width, self.width, k_size, 0) for i in range(ndims)])
 
         self.fc1 = nn.Linear(self.width, 512)
         self.fc2 = nn.Linear(512, 1)
 
+        # Add normalization layers
+        self.hidden_norms = nn.ModuleList([ToggleableGroupNorm(hidden_norm_groups, hidden_channels) for _ in range(n_layers-1)])
+        self.output_norm = ToggleableGroupNorm(out_norm_groups, out_channels)
+
+    # Confirmed: size_w is the "component dimension" containing u,v,w.
+    # Accepts ONLY: [B, C, X, Y, Z]  (channels-first with components in C; single input timestep)
     def forward(self, x):
+        # Normalize input to [B, X, Y, Z, W, T=1]
+        assert x.ndim == 5, f'Expected input of shape [B,C,X,Y,Z], got {tuple(x.shape)}'
+        x = x.permute(0, 2, 3, 4, 1)[..., None]   # -> [B,X,Y,Z,W=C,T=1]
+
         batchsize = x.shape[0]
         size_x, size_y, size_z, size_w = x.shape[1], x.shape[2], x.shape[3], x.shape[4]
 
         grid = self.get_grid(batchsize, size_x, size_y, size_z, size_w, x.device)
         x = torch.cat((x, grid), dim=-1)
 
-        x = self.fc0(x)
-        x = x.permute(0, 5, 1, 2, 3, 4)
+        x = self.fc0(x) # Dwyer: apparently linear layers require the feature dimension to be last...
+        x = x.permute(0, 5, 1, 2, 3, 4) # Dwyer: then it seems they move the feature dimension to the channel axis (for CNNs)
         coef = 1./self.nlayer
 
         for i in range(self.nlayer-1):
-
             x1 = self.convlayer[0](x)
             x2 = self.w[0](x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y, size_z, size_w)
-            x3 = self.u[0](x-x1)
+            x3 = self.u[0](x-x1) # Dwyer: why the subtraction?
             x = F.gelu(x1+x2+x3)*coef + x
-
+            x = self.hidden_norms[i](x)
 
         x1 = self.convlayer[0](x)
         x2 = self.w[0](x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y, size_z, size_w)
         x3 = self.u[0](x-x1)
         x = (x1+x2+x3)*coef + x
 
-        x = x.permute(0, 2, 3, 4, 5, 1)
+        x = x.permute(0, 2, 3, 4, 5, 1) # Dwyer: channel dimension to last again
         x = self.fc1(x)
         x = F.gelu(x)
-        x = self.fc2(x)
+        x = self.fc2(x).permute(0, 4, 1, 2, 3, 5)[..., 0] # Confirmed: x.shape=[batch, X, Y, Z, W, T=1] -> [batch, W, X, Y, Z]
+        x = self.output_norm(x)
         return x
 
     def get_grid(self, batchsize, size_x, size_y, size_z, size_w, device ):
@@ -187,134 +206,129 @@ class FNO4d(nn.Module):
 
         return torch.cat((gridx, gridy, gridz, gridw), dim=-1).to(device) #
 
-
-
-
 # configs
 ################################################################
 
-device = torch.device("cuda")
-#-------------------------------------------------------------------------------
-#tunning3
-modes = 8
-width = 80
-epochs = 100
-learning_rate = 0.001
-weight_decay_value = 1e-11
-nlayer = 40
+# deprecated training code (interface changed)
+if __name__ == '__main__':
+    device = torch.device("meta")
+    #-------------------------------------------------------------------------------
+    #tunning3
+    modes = 8
+    width = 80
+    epochs = 100
+    learning_rate = 0.001
+    weight_decay_value = 1e-11
+    nlayer = 40
 
-#---------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------
 
-batch_size = 4
-scheduler_step = 10
-scheduler_gamma = 0.5
+    batch_size = 4
+    scheduler_step = 10
+    scheduler_gamma = 0.5
 
-print(epochs, learning_rate, scheduler_step, scheduler_gamma)
+    print(epochs, learning_rate, scheduler_step, scheduler_gamma)
 
-
-runtime = np.zeros(2, )
-t1 = default_timer()
-
-
-################################################################
-# load data
-################################################################
-
-
-#-------------------------------------------------
-vor_data = np.load('../../fno_data/data_mave.npy') #
-vor_data = vor_data[...,0:3]
-vor_data = vor_data[0:20,...]
-
-
-vor_data = torch.from_numpy(vor_data) #
-print(vor_data.shape)
-
-
-input_list = []
-output_list = []
-
-for j in range(vor_data.shape[0]):
-    for i in range(vor_data.shape[1]-5):
-
-        input_list.append(vor_data[j,i:i+5,...])
-        output_6m5 = (vor_data[j,i+5,...]-vor_data[j,i+4,...])
-        output_list.append(output_6m5)
-
-### switch dimension
-
-input_set = torch.stack(input_list)
-output_set = torch.stack(output_list)
-input_set = input_set.permute(0,2,3,4,5,1)
-
-
-full_set = torch.utils.data.TensorDataset(input_set, output_set)
-train_dataset, test_dataset = torch.utils.data.random_split(full_set, [int(0.8*len(full_set)),
-                                                                       len(full_set)-int(0.8*len(full_set))])
-
-# Data loader
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size,
-                                           shuffle=True)
-
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size,
-                                          shuffle=False)
-
-################################################################
-# training and evaluation
-################################################################
-model = FNO4d(modes, modes, modes, modes, width, nlayer).to(device)
-
-
-print(count_params(model))
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay_value)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
-
-mse_train = []
-mse_test = []
-
-
-myloss = LpLoss()
-
-for ep in range(epochs):
-    model.train()
+    runtime = np.zeros(2, )
     t1 = default_timer()
-    for xx, yy in train_loader:
-
-        xx = xx.to(device)
-        yy = yy.to(device)
-        im = model(xx).to(device)
-
-        train_loss = myloss(im.reshape(im.shape[0], -1), yy.reshape(yy.shape[0], -1))
-
-        optimizer.zero_grad()
-        train_loss.backward()
-        optimizer.step()
-    mse_train.append(train_loss.item())
 
 
-    with torch.no_grad():
-        for xx, yy in test_loader:
+    ################################################################
+    # load data
+    ################################################################
+
+
+    # vor=vorticity
+    #-------------------------------------------------
+    # Dwyer: 21x400x32x33x16x4 (groups, time, x, y, z, channels)
+    # channel flow variables are: u,v,w and p.
+    vor_data = np.load('./IUFNO-CHL/data_chl_re180/data_mave.npy')
+    vor_data = vor_data[...,0:3]
+    vor_data = vor_data[0:20,...]
+
+    vor_data = torch.from_numpy(vor_data)
+    print(vor_data.shape)
+
+
+    input_list = []
+    output_list = []
+
+    for j in range(vor_data.shape[0]):
+        for i in range(vor_data.shape[1]-5):
+
+            input_list.append(vor_data[j,i:i+5,...])
+            output_6m5 = (vor_data[j,i+5,...]-vor_data[j,i+4,...])
+            output_list.append(output_6m5)
+
+    ### switch dimension
+
+    input_set = torch.stack(input_list)
+    output_set = torch.stack(output_list)
+    input_set = input_set.permute(0,2,3,4,5,1)
+
+    # GOTCHA: using a random split here is crazy
+    full_set = torch.utils.data.TensorDataset(input_set.to('meta'), output_set.to('meta'))
+    train_dataset, test_dataset = torch.utils.data.random_split(full_set, [int(0.8*len(full_set)),
+                                                                        len(full_set)-int(0.8*len(full_set))])
+
+    # Data loader
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                            batch_size=batch_size,
+                                            shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                            batch_size=batch_size,
+                                            shuffle=False)
+
+    ################################################################
+    # training and evaluation
+    ################################################################
+    model = FNO4d(modes, modes, modes, modes, width, nlayer).to(device)
+
+
+    print(count_params(model))
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay_value)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+
+    mse_train = []
+    mse_test = []
+
+
+    myloss = LpLoss()
+
+    for ep in range(epochs):
+        model.train()
+        t1 = default_timer()
+        for xx, yy in train_loader:
+
             xx = xx.to(device)
             yy = yy.to(device)
-
             im = model(xx).to(device)
-            test_loss = myloss(im.reshape(im.shape[0], -1), yy.reshape(yy.shape[0], -1))
-        mse_test.append(test_loss.item())
 
-    t2 = default_timer()
+            train_loss = myloss(im.reshape(im.shape[0], -1), yy.reshape(yy.shape[0], -1))
 
-
-    print(ep, "%.2f" % (t2 - t1), 'train_loss: {:.4f}'.format(train_loss.item()),
-          'test_loss: {:.4f}'.format(test_loss.item()))
-
-MSE_save=np.dstack((mse_train,mse_test)).squeeze()
-np.savetxt('./loss_IUFNO.dat',MSE_save,fmt="%16.7f")
-
-torch.save(model.state_dict(), 'weights_IUFNO.pth')
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+        mse_train.append(train_loss.item())
 
 
+        with torch.no_grad():
+            for xx, yy in test_loader:
+                xx = xx.to(device)
+                yy = yy.to(device)
+
+                im = model(xx).to(device)
+                test_loss = myloss(im.reshape(im.shape[0], -1), yy.reshape(yy.shape[0], -1))
+            mse_test.append(test_loss.item())
+
+        t2 = default_timer()
 
 
+        print(ep, "%.2f" % (t2 - t1), 'train_loss: {:.4f}'.format(train_loss.item()),
+            'test_loss: {:.4f}'.format(test_loss.item()))
 
+    MSE_save=np.dstack((mse_train,mse_test)).squeeze()
+    np.savetxt('./loss_IUFNO.dat',MSE_save,fmt="%16.7f")
+
+    torch.save(model.state_dict(), 'weights_IUFNO.pth')
